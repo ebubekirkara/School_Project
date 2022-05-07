f9d3cd994ade67bff75df75909ded7a17924668a
ebubekirkara <ekaraoglu.colombia@gmail.com>
2022-05-07 18:33:04 +0100

    second commit

---
+++ src/School.java
@@ -0,0 +1,11 @@
+public interface School {
+
+    public static final String SCHOOL_NAME = "Techproed";
+    public abstract String departmentName();
+
+    public abstract Instructor instructor();
+
+    public abstract Student student();
+
+
+}
